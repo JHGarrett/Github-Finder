fa5094b0a3599c040b677f05252e42e8f2dcaebf
John Garrett <infantrygarrett@icloud.com>
2019-07-19 20:37:13 -0500

    updated state for clear users

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
-  state = {
-    text: '',
-  };
+         state = {
+           text: '',
+         };
 
-  onSubmit = e => {};
+         static propTypes = {
+           searchUsers: PropTypes.func.isRequired,
+           clearhUsers: PropTypes.func.isRequired,
+         };
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+         onSubmit = e => {
+           e.preventDefault();
+           this.props.searchUsers(this.state.text);
+           this.setState({ text: '' });
+         };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className='form'>
-          <input
-            type='text'
-            name='text'
-            placeholder='Search Users...'
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type='submit'
-            value='Search'
-            className='btn btn-dark btn-block'
-          />
-        </form>
-      </div>
-    );
-  }
-}
+         onChange = e => {
+           this.setState({ [e.target.name]: e.target.value });
+         };
+
+         render() {
+           return (
+             <div>
+               <form onSubmit={this.onSubmit} className='form'>
+                 <input
+                   type='text'
+                   name='text'
+                   placeholder='Search Users...'
+                   value={this.state.text}
+                   onChange={this.onChange}
+                 />
+                 <input
+                   type='submit'
+                   value='Search'
+                   className='btn btn-dark btn-block'
+                 />
+               </form>
+               <button
+                 className='btn btn-light btn-block'
+                 onClick={this.props.clear}>
+                 Clear
+               </button>
+             </div>
+           );
+         }
+       }
 
 export default Search;
